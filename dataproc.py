c2e977576f6c3c40d32dc69b38d189d83da64c23
ravi <ravi.nitp.ece@gmail.com>
2017-04-06 17:30:25 -0400

    extract conversation in text files

--- dataproc.py
+++ dataproc.py
@@ -15,6 +15,38 @@ def getid_line_mapping():
 
 	return id2line
 
+def get_conversations():
+    conv_lines = open('raw_data/movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+    #print (conv_lines)
+    # exclude possible empty last character
+    conv_lines = conv_lines[:-1]
+    convs = [ ]
+    for line in conv_lines:
+    	split = line.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    	convs.append(split.split(','))
+    print ("printing conversations", convs)
+    return convs
+
+'''
+read convs created above
+and create and save each convs in 
+a new text file
+
+'''
+def create_conversations(convs,id2line,path=""):
+	f_id = 0
+	for conv in convs:
+		 f_conv = open(path + str(f_id)+'.txt', 'w')
+		 for id in conv:
+		 	f_conv.write(id2line[id])
+		 	f_conv.write('\n')
+		 f_conv.close()
+		 f_id +=1
 
 if __name__ == '__main__':
-	getid_line_mapping()
+	id2line = getid_line_mapping()
+	convs = get_conversations()
+	print (convs)
+	create_conversations(convs,id2line)
+
+
