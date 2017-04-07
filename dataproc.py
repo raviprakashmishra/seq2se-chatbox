30f9465d91ec6f6752f110ce1f3403e41eef070d
ravi <ravi.nitp.ece@gmail.com>
2017-04-07 00:56:06 -0400

    completed data processing

--- dataproc.py
+++ dataproc.py
@@ -1,4 +1,6 @@
 separator = " +++$+++ "
+EN_WHITELIST = '0123456789abcdefghijklmnopqrstuvwxyz ' # space is included in whitelist
+EN_BLACKLIST = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\''
 
 limit = {
         'maxq' : 25,
@@ -11,6 +13,14 @@ UNK = 'unk'
 VOCAB_SIZE = 8000
 
 import random
+
+import nltk
+import itertools
+from collections import defaultdict
+
+import numpy as np
+
+import pickle
 
 def getid_line_mapping():
 	
@@ -118,12 +128,66 @@ def filter_data(qseq, aseq):
 
     return filtered_q, filtered_a
 
-def index_(tokenized_sentences, vocab_size):
+def index_data(tokenized_sentences, vocab_size):
     freq_dist = nltk.FreqDist(itertools.chain(*tokenized_sentences))
     vocab = freq_dist.most_common(vocab_size)
     index2word = ['_'] + [UNK] + [ x[0] for x in vocab ]
     word2index = dict([(w,i) for i,w in enumerate(index2word)] )
     return index2word, word2index, freq_dist
+
+def filter_unk(qtokenized, atokenized, w2idx):
+    data_len = len(qtokenized)
+
+    filtered_q, filtered_a = [], []
+
+    for qline, aline in zip(qtokenized, atokenized):
+        unk_count_q = len([ w for w in qline if w not in w2idx ])
+        unk_count_a = len([ w for w in aline if w not in w2idx ])
+        if unk_count_a <= 2:
+            if unk_count_q > 0:
+                if unk_count_q/len(qline) > 0.2:
+                    pass
+            filtered_q.append(qline)
+            filtered_a.append(aline)
+
+    
+
+    return filtered_q, filtered_a
+
+
+
+
+
+def zero_pad(qtokenized, atokenized, w2idx):
+    # num of rows
+    data_len = len(qtokenized)
+
+    # numpy arrays to store indices
+    idx_q = np.zeros([data_len, limit['maxq']], dtype=np.int32) 
+    idx_a = np.zeros([data_len, limit['maxa']], dtype=np.int32)
+
+    for i in range(data_len):
+        q_indices = pad_seq(qtokenized[i], w2idx, limit['maxq'])
+        a_indices = pad_seq(atokenized[i], w2idx, limit['maxa'])
+
+     
+        idx_q[i] = np.array(q_indices)
+        idx_a[i] = np.array(a_indices)
+
+    return idx_q, idx_a
+
+
+def pad_seq(seq, lookup, maxlen):
+    indices = []
+    for word in seq:
+        if word in lookup:
+            indices.append(lookup[word])
+        else:
+            indices.append(lookup[UNK])
+    return indices + [0]*(maxlen - len(seq))
+
+def filter_line(line, whitelist):
+    return ''.join([ ch for ch in line if ch in whitelist ])
 
 if __name__ == '__main__':
 	id2line = getid_line_mapping()
@@ -131,6 +195,33 @@ if __name__ == '__main__':
 	print (convs)
 	create_conversations(convs,id2line)
 	q,a = gather_dataset(convs,id2line)
-	prepare_seq2seq_files(q,a)
+	#prepare_seq2seq_files(q,a)
+	q = [line.lower() for line in q]
+	a = [line.lower() for line in a]
 
+	questions = [ filter_line(line, EN_WHITELIST) for line in q ]
+	answers = [ filter_line(line, EN_WHITELIST) for line in a ]
+
+	qlines, alines = filter_data(questions, answers)
+
+	qtokenized = [ [w.strip() for w in wordlist.split(' ') if w] for wordlist in qlines ]
+	atokenized = [ [w.strip() for w in wordlist.split(' ') if w] for wordlist in alines ]
+
+	idx2w, w2idx, freq_dist = index_data( qtokenized + atokenized, vocab_size=VOCAB_SIZE)
+
+	qtokenized, atokenized = filter_unk(qtokenized, atokenized, w2idx)
+	idx_q, idx_a = zero_pad(qtokenized, atokenized, w2idx)
+	np.save('idx_q.npy', idx_q)
+	np.save('idx_a.npy', idx_a)
+
+	metadata = {
+            'w2idx' : w2idx,
+            'idx2w' : idx2w,
+            'limit' : limit,
+            'freq_dist' : freq_dist
+                }
+
+    # write to disk : data control dictionaries
+	with open('metadata.pkl', 'wb') as f:
+		pickle.dump(metadata, f)
 
